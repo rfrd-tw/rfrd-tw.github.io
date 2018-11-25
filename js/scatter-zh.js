8178b61b97169358330076b12e81d17f69109401
claire <amoss.claire@gmail.com>
2018-11-25 16:18:30 -0600

    updated data & vote_num selection function

--- js/scatter-zh.js
+++ js/scatter-zh.js
@@ -1,19 +1,17 @@
-var vote_num = 7;
-function chooseScatter(x) {
-	vote_num = x;
-}
+var vote_num = 7,
+chooseScatter,
+width = parseInt($('.scatter-plot').width()),
+height = parseInt($('.scatter-plot').height()),
+margin = {
+	"top": parseInt($('.scatter-plot').css('margin-top').replace(/px/g, "")),
+	"right": parseInt($('.scatter-plot').css('margin-right').replace(/px/g, "")),
+	"bottom": parseInt($('.scatter-plot').css('margin-bottom').replace(/px/g, "")),
+	"left": parseInt($('.scatter-plot').css('margin-left').replace(/px/g, ""))
+};
 
 d3.csv("data/data_zh.csv").then(function(data) {
 
-	const draw_scp = (vote, ...ind_vars) => {
-		var width = parseInt($('.scatter-plot').width()),
-		height = parseInt($('.scatter-plot').height()),
-		margin = {
-			"top": parseInt($('.scatter-plot').css('margin-top').replace(/px/g, "")),
-			"right": parseInt($('.scatter-plot').css('margin-right').replace(/px/g, "")),
-			"bottom": parseInt($('.scatter-plot').css('margin-bottom').replace(/px/g, "")),
-			"left": parseInt($('.scatter-plot').css('margin-left').replace(/px/g, ""))
-		};
+	draw_scp = function draw_scp(vote, ...ind_vars) {
 
 		tooltip = d3.select("body").append("div")
         .attr("class", "tooltip-scp")
@@ -33,6 +31,8 @@ d3.csv("data/data_zh.csv").then(function(data) {
 		z = d3.scaleThreshold()
 		.domain(0.5)
 		.range(["#e83e8c", "#9a55fc"]);
+
+		var yAxisSet1, yAxisSet2;
 
 		ind_vars.forEach(function(ind_var, i) {
 
@@ -62,13 +62,14 @@ d3.csv("data/data_zh.csv").then(function(data) {
 			.attr("width", width + margin.left + margin.right)
 			.attr("height", height + margin.top + margin.bottom)
 			.append("g")
-			.attr("transform", "translate(" + margin.left + "," + margin.top + ")");
+			.attr("transform", "translate(" + margin.left + "," + margin.top + ")")
+			.attr("class", "plot-g");
 
 			// y is customized by input variables
 			y = d3.scaleLinear()
 			.range([height - margin.bottom, margin.top]);
 
-			var yAxisSet1 = d3.axisLeft(y), yAxisSet2;
+			yAxisSet1 = d3.axisLeft(y);
 			if (d3.max(data, d => (Number(d[ind_var]))) < 1 ) {
 				yAxisSet2 = yAxisSet1.tickFormat(d3.format(".0%"));
 				y.domain([d3.min(data, d => (Number(d[ind_var])-0.02)), d3.max(data, d => (Number(d[ind_var])+0.001))]).nice();
@@ -96,14 +97,14 @@ d3.csv("data/data_zh.csv").then(function(data) {
 				.attr("font-weight", "bold"));
 
 			svg.append("g")
+			.attr("class", "xAxis")
 			.call(xAxis);
 
 			svg.append("g")
 			.call(yAxis);
 
 			circle = svg.append("g")
-			.attr("stroke", "#000")
-			.attr("stroke-opacity", 0.2)
+			.attr("class", "circles")
 			.selectAll("circle")
 			.data(data)
 			.enter().append("circle")
@@ -119,14 +120,6 @@ d3.csv("data/data_zh.csv").then(function(data) {
 			// hide null
 			circle.style("display", function(d) { return d[vote] == "NA" ? "none" : null; });
 
-			svg.append("line")
-			.attr("x1", x(0.5))  
-			.attr("y1", 0)
-			.attr("x2", x(0.5))  
-			.attr("y2", height - margin.top - margin.bottom)
-			.style("stroke-width", 1)
-			.style("stroke", "#D3D3D3")
-			.style("fill", "none");
 		}); // end of ind_vars.forEach()
 
 		d3.selection.prototype.moveToFront = function() {
@@ -146,11 +139,9 @@ d3.csv("data/data_zh.csv").then(function(data) {
 			.style("left", (d3.event.pageX + 10) + "px")
             .style("top", (d3.event.pageY - 28) + "px");
 
-            console.log(d);
-
 			tooltip
 			.transition()
-			.duration(500)
+			.duration(300)
 			.style("opacity", 1);
 		}
 
@@ -166,7 +157,7 @@ d3.csv("data/data_zh.csv").then(function(data) {
 
 			tooltip
 			.transition()
-			.duration(500)
+			.duration(300)
 			.style("opacity", 1);
 		}
 
@@ -180,7 +171,62 @@ d3.csv("data/data_zh.csv").then(function(data) {
             .style("opacity", 0);
 		}
 
+		chooseScatter = function chooseScatter(num) {
+			
+			console.log(num);
+			vote_num = num;
+			vote = "vote"+vote_num;
+
+			x.domain([d3.min(data, d => (Number(d[vote])-0.02)), d3.max(data, d => (Number(d[vote])+0.02))]).nice();
+
+			ind_vars.forEach(function(ind_var, i) {
+
+				if (d3.max(data, d => (Number(d[ind_var]))) < 1 ) {
+					yAxisSet2 = yAxisSet1.tickFormat(d3.format(".0%"));
+					y.domain([d3.min(data, d => (Number(d[ind_var])-0.02)), d3.max(data, d => (Number(d[ind_var])+0.001))]).nice();
+				} else {
+					yAxisSet2 = yAxisSet1;
+					y.domain([d3.min(data, d => (Number(d[ind_var])-0.02)), d3.max(data, d => (Number(d[ind_var])+0.1))]).nice();
+				};
+
+				var curPlot = d3.select("#scp"+i+"-plot").select("svg");
+
+				curPlot.select(".xAxis")
+				.transition()
+				.duration(500)
+				.call(xAxis);
+
+				curPlot.select(".circles").remove(); //remove unneeded circles
+
+				newCircle = curPlot.append("g")
+				.attr("class", "circles")
+				.selectAll("circle")
+				.data(data)
+				.enter().append("circle")
+				.attr("class", ind_var)
+				.attr("cx", d => x(d[vote]))
+				.attr("cy", d => y(d[ind_var]))
+				.attr("fill", "#e83e8c")
+				.attr("r", 0)
+				.attr("id", d => d.site_id)
+				.on("mouseover", mouseover)
+				.on("click", click)
+				.on("mouseout", mouseout);
+
+				newCircle.transition()
+				.duration(800)
+				.attr("r", 2.5);
+
+			// hide null
+			newCircle.style("display", function(d) { return d[vote] == "NA" ? "none" : null; });
+
+		}); // end of update -> ind_vars.forEach()
+
+		}
+
 		//return svg.node();
 	}
-	draw_scp("vote"+vote_num, "大學畢業比例(%)", "曾經結婚比例(%)", "年齡中位數", "所得中位數（萬元）"); 
+
+	draw_scp("vote"+vote_num, "大學畢業比例(%)","曾經結婚比例(%)", "年齡中位數", "所得中位數(萬元)"); 
+
 });
